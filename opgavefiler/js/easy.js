4b0cb1a182052dd006aa12c6d41b966ffbe33cab
Indo Krish <55687559+Indok93@users.noreply.github.com>
2023-09-27 09:47:26 +0200

    easy done

--- opgavefiler/js/easy.js
+++ opgavefiler/js/easy.js
@@ -1,7 +1,14 @@
 /* opgave 1 */
 console.group("opgave 1");
 // din kode her
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+    const targetDom = document.getElementById('opgave1');
+    let numberList = document.createElement('li');
+    numberList.innerHTML = i;
 
+    targetDom.appendChild(numberList);
+}
 
 console.groupEnd();
 
@@ -9,7 +16,14 @@ console.groupEnd();
 /* opgave 2 */
 console.group("opgave 2");
 // din kode her
+for (let i = 10; i < 12; i++) {
+    console.log(i);
+    const targetDom = document.getElementById('opgave2');
+    let numberList = document.createElement('li');
+    numberList.innerHTML = i;
 
+    targetDom.appendChild(numberList);
+}
 
 console.groupEnd();
 
@@ -17,7 +31,15 @@ console.groupEnd();
 /* opgave 3 */
 console.group("opgave 3");
 // din kode her
+for (let i = 2; i < 41; i++) {
+    console.log(i);
+    const targetDom = document.getElementById('opgave3');
+    let numberList = document.createElement('li');
+    numberList.innerHTML = i;
 
+    targetDom.appendChild(numberList);
+    
+}
 
 console.groupEnd();
 
